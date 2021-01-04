42b7f093d7de504b740b4e098753fe05a6574595
Marchael Acode <m.acode@outlook.com>
2021-01-04 14:26:42 -0800

    Drawing player and ai rectangle

--- src/Window.java
+++ src/Window.java
@@ -1,11 +1,13 @@
 import javax.swing.JFrame;
 import java.awt.*;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 
 public class Window extends JFrame implements Runnable {
 
     Graphics2D g2;
     KL keyListener = new KL();
+    Rect playerOne, ai, ball;
 
     public Window() {
         this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
@@ -15,17 +17,36 @@ public class Window extends JFrame implements Runnable {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.addKeyListener(keyListener);
         g2 = (Graphics2D) this.getGraphics();
+
+        playerOne = new Rect(Constants.HZ_PADDING,
+                35,
+                Constants.PADDLE_WIDTH,
+                Constants.PADDLE_HEIGHT,
+                Constants.PADDLE_COLOR);
+        ai = new Rect(Constants.SCREEN_WIDTH - Constants.PADDLE_WIDTH - Constants.HZ_PADDING,
+                35,
+                Constants.PADDLE_WIDTH,
+                Constants.PADDLE_HEIGHT, Constants.PADDLE_COLOR);
+        ball = new Rect(Constants.SCREEN_WIDTH / 2,
+                Constants.SCREEN_HEIGHT / 2,
+                Constants.BALL_WIDTH,
+                Constants.PADDLE_WIDTH,
+                Constants.PADDLE_COLOR);
     }
 
     public void update(double dt) {
         g2.setColor(Color.BLACK);
         g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
 
-        if (keyListener.isKeyPressed(KeyEvent.VK_UP)) {
-            System.out.println("The user is pressing the up arrow");
-        } else if (keyListener.isKeyPressed(KeyEvent.VK_DOWN)) {
-            System.out.println("The user is pressing the down arrow");
-        }
+        playerOne.draw(g2);
+        ai.draw(g2);
+        ball.draw(g2);
+
+//        if (keyListener.isKeyPressed(KeyEvent.VK_UP)) {
+//            System.out.println("The user is pressing the up arrow");
+//        } else if (keyListener.isKeyPressed(KeyEvent.VK_DOWN)) {
+//            System.out.println("The user is pressing the down arrow");
+//        }
     }
 
     public void run() {
